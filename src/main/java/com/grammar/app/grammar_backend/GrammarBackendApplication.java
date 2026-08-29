e738a62db2c4e275dd68b29766362baf173d9401
Arjun Iyer <iarjun2001@gmail.com>
2026-08-29 16:16:06 -0400

    Initial repo structure

---
+++ src/main/java/com/grammar/app/grammar_backend/GrammarBackendApplication.java
@@ -0,0 +1,13 @@
+package com.grammar.app.grammar_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrammarBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrammarBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grammar/app/grammar_backend/GrammarBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grammar.app.grammar_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrammarBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
